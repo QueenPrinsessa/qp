1d834a0f9e52f1041a6a3940836e922c67a2c541
QueenPrinsessa <57677342+QueenPrinsessa@users.noreply.github.com>
2023-11-11 11:23:07 +0100

    qpClock: Includes inside #ifdef

--- qp/common/platform/windows/time/qp_windows_clock.cpp
+++ qp/common/platform/windows/time/qp_windows_clock.cpp
@@ -1,5 +1,6 @@
-#include "qp/common/core/qp_macros.h"
 #ifdef QP_PLATFORM_WINDOWS
+
+#include "qp/common/core/qp_macros.h"
 #include "qp/common/time/qp_time_point.h"
 #include "qp/common/time/qp_clock.h"
 #include "qp/common/platform/windows/qp_windows.h"
