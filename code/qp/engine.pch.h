5fbece8cd8a633cc60ef25c665d8cf504b65ae6b
QueenPrinsessa <57677342+QueenPrinsessa@users.noreply.github.com>
2024-01-19 12:41:58 +0100

    Added some common includes to engine.pch.h

--- code/qp/engine.pch.h
+++ code/qp/engine.pch.h
@@ -1,1 +1,8 @@
-﻿
+﻿#pragma once
+
+#include <cstddef>
+#include <cstring>
+
+#include "qp/common/debug/qp_debug.h"
+#include "qp/common/utilities/qp_algorithms.h"
+#include "qp/common/utilities/qp_utility.h"
