f149ef3a46e1d9a26b320870b0c370db25733ffe
QueenPrinsessa <57677342+QueenPrinsessa@users.noreply.github.com>
2024-04-29 21:01:29 +0200

    Linux: Added options to compile.sh script.

--- compile.sh
+++ compile.sh
@@ -1,2 +1,24 @@
 sh gen_make.sh
-make config=debug_linux_vulkan_dev_x64
+
+config=debug
+platform=linux
+renderer=vulkan
+dev=0
+
+while getopts c:p:d:r:h flag
+do
+	case "${flag}" in
+		c) config=${OPTARG};;
+		p) platform=${OPTARG};;
+		d) dev=1;;
+		r) renderer=${OPTARG};;
+		h) renderer=headless;;
+	esac
+done
+
+if [ "$dev" -eq 0 ]
+then
+	make config="${config}_${platform}_${renderer}_x64"
+else
+	make config="${config}_${platform}_${renderer}_dev_x64"
+fi
