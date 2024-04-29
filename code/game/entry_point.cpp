cdd03356c4fe5abfb4485371b6c0cefbf6fde7d4
QueenPrinsessa <57677342+QueenPrinsessa@users.noreply.github.com>
2024-04-29 21:19:41 +0200

    Linux: Added entry point, and some example code to the headless app

--- code/game/entry_point.cpp
+++ code/game/entry_point.cpp
@@ -43,4 +43,38 @@ int WINAPI wWinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLin
 	return 0;
 }
 
+#elif defined( QP_PLATFORM_LINUX )
+int main() {
+#if !defined( QP_RETAIL )
+	Sys_InitializeConsole();
+#endif
+
+#if defined( QP_HEADLESS )
+	qpHeadlessApp app;
+#else
+	windowProperties_t windowProperties;
+	windowProperties.width = 800;
+	windowProperties.height = 600;
+	windowProperties.allowResize = true;
+	windowProperties.mode = windowMode_t::WINDOWED;
+	windowProperties.title = "qpVulkanLinuxWindow";
+
+	// windowPropertiesWindows_t windowsProperties;
+	// windowsProperties.instanceHandle = hInstance;
+
+	//windowProperties.platformData = &windowsProperties;
+
+	qpWindowedApp app( windowProperties );
+#endif
+
+	try {
+		app.Run();
+	} catch ( const std::exception & e ) {
+		qpLog::Error( "%s", e.what() );
+	}
+
+	return 0;
+}
+#else
+#error There's no entrypoint defined for platform!
 #endif
--- code/qp/engine/core/qp_headless_app.cpp
+++ code/qp/engine/core/qp_headless_app.cpp
@@ -1,11 +1,21 @@
 #include "engine.pch.h"
 #include "qp_headless_app.h"
+#include "qp/engine/debug/qp_log.h"
 
 void qpHeadlessApp::OnInit () {
+    qpLog::Printf( "%s\n", __FUNCTION__ );
 }
 
 void qpHeadlessApp::OnUpdate () {
+    qpLog::Printf( "%s\n", __FUNCTION__ );
+    qpLog::Printf( "Type 'q' to shutdown.\n" );
+    char input = -1;
+    std::cin >> input;
+    if ( input == 'q' ) {
+        RequestShutdown();
+    }
 }
 
 void qpHeadlessApp::OnCleanup () {
+    qpLog::Printf( "%s\n", __FUNCTION__ );
 }
